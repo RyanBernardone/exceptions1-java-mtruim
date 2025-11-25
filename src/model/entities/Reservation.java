8be24d5054668969839b7c2fdb82492cf0140048
Ryan <ryan.bernardone@gmail.com>
2025-11-24 21:05:13 -0300

    feat: criado a estrutura inicial de pastas e as classes principais

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,57 @@
+package model.entities;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Quarto: ").append(roomNumber);
+        sb.append(", check-in em: ").append(sdf.format(checkIn));
+        sb.append(" com check-out em: ").append(sdf.format(checkOut));
+        sb.append(". Duração de ").append(duration()).append("noites");
+        return sb.toString();
+    }
+}
